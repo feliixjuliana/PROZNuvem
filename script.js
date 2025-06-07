5454a8537fe3e8b34b58cde05815133f61605b0c
Juliana Felix <132230500+feliixjuliana@users.noreply.github.com>
2025-06-07 11:58:49 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function mostrarMensagem() {
+    alert("Olá, sou juliana, fico grata que queira ver meu linkedIn.")
+}
+
+function verificandoNumero() {
+
+    let numero = document.getElementById("numero").value
+
+    numero = Number(numero);
+    let mensagem = ""
+
+    if (numero == 0) {
+        mensagem = "Número neutro"
+    } else if (numero > 0) {
+        mensagem = "Número positivo"
+    } else {
+        mensagem = "Número negativo"
+    }
+
+document.getElementById("resultado").innerTEXT = mensagem
+}
