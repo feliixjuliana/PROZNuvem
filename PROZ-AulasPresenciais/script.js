15bff7df72a0375c460bc491364127a06fac96ec
juliana <user.taniajuju91@gmail.com>
2025-06-18 10:31:48 -0300

    Style: Background MarcoZero

--- PROZ-AulasPresenciais/script.js
+++ PROZ-AulasPresenciais/script.js
@@ -5,7 +5,6 @@ function mostrarMensagem() {
 function verificandoNumero() {
 
     let numero = document.getElementById("numero").value
-
     numero = Number(numero);
     let mensagem = ""
 
@@ -17,5 +16,29 @@ function verificandoNumero() {
         mensagem = "Número negativo"
     }
 
-document.getElementById("resultado").innerHTML = mensagem
+document.getElementById("resultado").innerText = mensagem
+};
+
+function executarFor(){
+    let resultado = "Número pares 0 a 10 \n -> \n";
+
+    for (let i = 0; i <=10; i++){
+        if(i % 2 === 0){
+           resultado += i + '\n';
+        }
+
+    }
+
+    document.getElementById("saída").textContent = resultado
+}
+
+function executarArray(){
+    let nomes = ["Manu", "Samara", "Andreia", "Ana"];
+    let resultado = "Lista de Alunas: \n"
+
+    for(i = 0; i < nomes.length; i++){
+        resultado += nomes[i] + '\n';
+    }
+
+    document.getElementById("saida").textContent = resultado;
 }
